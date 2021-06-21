c3227ffe75f7166c95f2bd0e86e50ecbc4b5430a
waiwai888 <yuisato9688@gmail.com>
2021-06-21 19:51:44 +0900

    [start]js6

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 $(function (){
-    $('.box1').slideDown(function (){
-      $('.box1').css({
-        'background-color': '#0000FF',
-        'width': '200px',
-        'height': '100px'
-      }).slideUp();
+    $('.box1').mouseover(function(){
+        $('.box1').css({
+            'background-color':'#0000FF'
+        });
     });
-  });
+});
